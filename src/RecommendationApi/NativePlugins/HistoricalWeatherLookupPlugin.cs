9e6fbdc9028dffa889de821bb1bf8ca53c2e4ec3
Jordan Bean <jordanbean@microsoft.com>
2023-11-13 13:24:29 -0600

    logger

--- src/RecommendationApi/NativePlugins/HistoricalWeatherLookupPlugin.cs
+++ src/RecommendationApi/NativePlugins/HistoricalWeatherLookupPlugin.cs
@@ -13,13 +13,13 @@ namespace RecommendationApi.Plugins
         }
 
         [SKFunction, Description("Get the historical weather for a location for a month. Make sure and pass in the month of the year the user requested. The weather temperatures will be returned in Fahrenheit. Not Found will be returned if the GPS coordinates or the month of the year are not valid. Make sure and pass in valid GPS coordinates for the location requested by the user.")]
-        public async Task<string> HistoricalWeatherLookup(
+        public async Task<string> HistoricalWeatherLookupAsync(
             [Description("The latitude of the GPS location to lookup historical weather for. This should be a string, not JSON.")] double latitude,
             [Description("The longitude of the GPS location to lookup historical weather for. This should be a string, not JSON.")] double longitude,
-            [Description("The integer month of the year to lookup historical weather for")] int monthOfYear)
+            [Description("The integer month of the year to lookup historical weather for")] int monthOfYear, ILogger logger, CancellationToken cancellationToken)
         {
             var httpRequest = _daprClient.CreateInvokeMethodRequest(HttpMethod.Get, "historical-weather-lookup", $"historical-weather-lookup?latitude={latitude}&longitude={longitude}&monthOfYear={monthOfYear}");
-            HttpResponseMessage result = await _daprClient.InvokeMethodWithResponseAsync(httpRequest);
+            HttpResponseMessage result = await _daprClient.InvokeMethodWithResponseAsync(httpRequest, cancellationToken);
 
             if (result.IsSuccessStatusCode)
             {
