ca68a3af1c55be53f652fca0d33a7b2615160f35
Jordan Bean <jordanbean@microsoft.com>
2023-12-19 17:01:10 -0600

    graph

--- src/recommendation-web-app/src/components/ResponseGraph/ResponseGraph.tsx
+++ src/recommendation-web-app/src/components/ResponseGraph/ResponseGraph.tsx
@@ -39,23 +39,28 @@ function ConvertToMermaidDiagram(response: ResponseMessage) {
     response.chatHistory.forEach((message, index, chatHistory) => {
         if (message.role === "system") {
             chatMessages += `    WebApp->>OpenAI: ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}\n`;
+            chatMessages += `    Note over WebApp,OpenAI: ${message.promptTokens}/${message.completionTokens}/${message.totalTokens}\n`
         }
 
         if (message.role === "assistant") {
             if (message.functionName != "") {
                 chatMessages += `    WebApp->>${message.functionName}: ${message.functionArguments.replace(/(?:\r\n|\r|\n)/g, '')}\n`;
+                chatMessages += `    Note over WebApp,${message.functionName}: ${message.promptTokens}/${message.completionTokens}/${message.totalTokens}\n`
             }
             else {
                 chatMessages += `    WebApp->>OpenAI: ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}\n`;
+                chatMessages += `    Note over WebApp,OpenAI: ${message.promptTokens}/${message.completionTokens}/${message.totalTokens}\n`
             }
         }
 
         if (message.role === "user") {
             chatMessages += `    OpenAI->>WebApp: ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}\n`;
+            chatMessages += `    Note over OpenAI,WebApp: ${message.promptTokens}/${message.completionTokens}/${message.totalTokens}\n`
         }
 
         if (message.role === "tool") {
             chatMessages += `    ${chatHistory[index - 1].functionName}->>WebApp: ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}\n`;
+            chatMessages += `    Note over ${chatHistory[index - 1].functionName},WebApp: ${message.promptTokens}/${message.completionTokens}/${message.totalTokens}\n`
         }
     }
     );
@@ -63,8 +68,8 @@ function ConvertToMermaidDiagram(response: ResponseMessage) {
     let mermaidDiagram =
     `sequenceDiagram
          box Semantic Kernel
-         participant WebApp
          participant OpenAI
+         participant WebApp
          end
          box Plugins
          ${pluginFunctions}
