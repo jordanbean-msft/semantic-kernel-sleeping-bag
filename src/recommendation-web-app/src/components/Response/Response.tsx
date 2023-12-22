03d4fd21735538930afa5f6e934c89e5c3348678
Jordan Bean <jordanbean@microsoft.com>
2023-12-22 08:31:32 -0600

    formatting

--- src/recommendation-web-app/src/components/Response/Response.tsx
+++ src/recommendation-web-app/src/components/Response/Response.tsx
@@ -13,92 +13,95 @@ import IconButton from "@mui/material/IconButton";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import ResponseGraph from "../ResponseGraph/ResponseGraph";
+import Container from "@mui/material/Container";
 
 export interface OpenAIMessage {
-  content: string;
-  role: string;
-  functionName: string;
-  functionArguments: string;
-  completionTokens: number;
-  promptTokens: number;
-  totalTokens: number;
+    content: string;
+    role: string;
+    functionName: string;
+    functionArguments: string;
+    completionTokens: number;
+    promptTokens: number;
+    totalTokens: number;
 }
 
 export interface ResponseMessage {
-  iterations: number;
-  chatHistory: Array<OpenAIMessage>;
-  finalAnswer: string;
+    iterations: number;
+    chatHistory: Array<OpenAIMessage>;
+    finalAnswer: string;
 }
 
 export default function Response({ response }: { response: ResponseMessage }) {
     return (
         <div>
-            <ResponseGraph response={ response } />
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Role</TableCell>
-            <TableCell>Function Name</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {response?.chatHistory?.map(
-            (row: { content: React.Key | null | undefined }) => (
-              <Row row={row} />
-            )
-          )}
-        </TableBody>
-      </Table>
+            <Container maxWidth={false}>
+                <ResponseGraph response={response} />
+            </Container>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell>Role</TableCell>
+                            <TableCell>Function Name</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {response?.chatHistory?.map(
+                            (row: { content: React.Key | null | undefined }) => (
+                                <Row row={row} />
+                            )
+                        )}
+                    </TableBody>
+                </Table>
             </TableContainer>
-    </div>
-  );
+        </div>
+    );
 }
 
 function Row(props: { row: any }) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
 
-  return (
-    <React.Fragment>
-      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell scope="row">{row.role}</TableCell>
-        <TableCell>{row.functionName}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Role</TableCell>
-                    <TableCell>Content</TableCell>
-                    <TableCell>FunctionName</TableCell>
-                    <TableCell>FunctionArguments</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableCell scope="row">{row.role}</TableCell>
-                  <TableCell>{row.content}</TableCell>
-                  <TableCell>{row.functionName}</TableCell>
-                  <TableCell>{row.functionArguments}</TableCell>
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell scope="row">{row.role}</TableCell>
+                <TableCell>{row.functionName}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Role</TableCell>
+                                        <TableCell>Content</TableCell>
+                                        <TableCell>FunctionName</TableCell>
+                                        <TableCell>FunctionArguments</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableCell scope="row">{row.role}</TableCell>
+                                    <TableCell>{row.content}</TableCell>
+                                    <TableCell>{row.functionName}</TableCell>
+                                    <TableCell>{row.functionArguments}</TableCell>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
 }
--- src/recommendation-web-app/src/components/ResponseGraph/ResponseGraph.tsx
+++ src/recommendation-web-app/src/components/ResponseGraph/ResponseGraph.tsx
@@ -29,11 +29,11 @@ function ConvertToMermaidDiagram(response: ResponseMessage) {
     let semanticFunctions = "";
 
     response.chatHistory.forEach((message) => {
-        if((message.role === "assistant" && message.functionName != "" && !message.functionName.includes("_")) || message.functionName == "UserInteraction_SendFinalAnswer") {
+        if((message.role === "assistant" && message.functionName !== "" && !message.functionName.includes("_")) || message.functionName === "UserInteraction_SendFinalAnswer") {
             semanticFunctionNames.add(message.functionName);
         }
 
-        if (message.role === "assistant" && message.functionName != "" && message.functionName.includes("_") && message.functionName != "UserInteraction_SendFinalAnswer") {
+        if (message.role === "assistant" && message.functionName !== "" && message.functionName.includes("_") && message.functionName !== "UserInteraction_SendFinalAnswer") {
             pluginFunctionNames.add(message.functionName);
         }
 
@@ -49,32 +49,37 @@ function ConvertToMermaidDiagram(response: ResponseMessage) {
 
     response.chatHistory.forEach((message, index, chatHistory) => {
         if (message.role === "system") {
-            chatMessages += `    WebApp->>OpenAI: ${addNewLineAt80Characters(`${message.role} - ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>').trimEnd()}`)}\n`;
+            chatMessages += `    OpenAI->>WebApp: ${addNewLineAt80Characters(`${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>').trimEnd()}`)}\n`;
+            chatMessages += `    Note over OpenAI,WebApp: ${message.role}\n`
         }
 
         if (message.role === "assistant") {
-            if (message.functionName != "") {
-                chatMessages += `    OpenAI->>WebApp: ${addNewLineAt80Characters(`${message.role} - ${message.functionName} - ${message.functionArguments.replace(/(?:\r\n|\r|\n)/g, '')}`)}\n`;
+            if (message.functionName !== "") {
+                chatMessages += `    OpenAI->>WebApp: ${addNewLineAt80Characters(`${message.functionName} - ${message.functionArguments.replace(/(?:\r\n|\r|\n)/g, '')}`)}\n`;
                 if (message.totalTokens > 0) {
-                    chatMessages += `    Note left of OpenAI: P:${message.promptTokens}/C:${message.completionTokens}/T:${message.totalTokens}\n`
+                    chatMessages += `    Note over OpenAI,WebApp: ${message.role} - P:${message.promptTokens}/C:${message.completionTokens}/T:${message.totalTokens}\n`
                 }
-                chatMessages += `    WebApp->>+${message.functionName}: ${addNewLineAt80Characters(`${message.role} - ${message.functionArguments.replace(/(?:\r\n|\r|\n)/g, '')}`)}\n`;
+                chatMessages += `    WebApp->>${message.functionName}: ${addNewLineAt80Characters(`${message.functionArguments.replace(/(?:\r\n|\r|\n)/g, '')}`)}\n`;
+                chatMessages += `    Note over WebApp,${message.functionName}: ${message.role}\n`
             }
             else {
-                chatMessages += `    WebApp->>OpenAI: ${addNewLineAt80Characters(`${message.role} - ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+                chatMessages += `    OpenAI->>WebApp: ${addNewLineAt80Characters(`${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
                 if (message.totalTokens > 0) {
-                    chatMessages += `    Note right of OpenAI: P:${message.promptTokens}/C:${message.completionTokens}/T:${message.totalTokens}\n`
+                    chatMessages += `    Note over OpenAI,WebApp: ${message.role} - P:${message.promptTokens}/C:${message.completionTokens}/T:${message.totalTokens}\n`
                 }
             }
         }
 
         if (message.role === "user") {
-            chatMessages += `    WebApp->>WebApp: ${addNewLineAt80Characters(`${message.role} - ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+            chatMessages += `    WebApp->>WebApp: ${addNewLineAt80Characters(`${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+            chatMessages += `    Note over WebApp,WebApp: ${message.role}\n`;
         }
 
         if (message.role === "tool") {
-            chatMessages += `    ${chatHistory[index - 1].functionName}->>-WebApp: ${addNewLineAt80Characters(`${message.role} - ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
-            chatMessages += `    WebApp->>OpenAI: ${addNewLineAt80Characters(`${message.role} - ${chatHistory[index - 1].functionName} ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+            chatMessages += `    ${chatHistory[index - 1].functionName}->>WebApp: ${addNewLineAt80Characters(`${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+            chatMessages += `    Note over ${chatHistory[index - 1].functionName},WebApp: ${message.role}\n`;
+            chatMessages += `    WebApp->>OpenAI: ${addNewLineAt80Characters(`${chatHistory[index - 1].functionName} ${message.content?.replace(/(?:\r\n|\r|\n)/g, '<br/>')}`)}\n`;
+            chatMessages += `    Note over WebApp,OpenAI: ${message.role}\n`;
         }
     }
     );
@@ -82,8 +87,8 @@ function ConvertToMermaidDiagram(response: ResponseMessage) {
     let mermaidDiagram =
     `sequenceDiagram
          box OpenAI Semantic Functions
-         participant OpenAI
          ${semanticFunctions}
+         participant OpenAI
          end
          participant WebApp
          box Native Plugins
@@ -100,8 +105,9 @@ function addNewLineAt80Characters(input: string) {
     let result = '';
     for (let i = 0; i < input.length; i++) {
         if (i > 0 && i % 80 === 0) {
-            result += '<br/>';
+            result += '<br/>'
         }
+
         result += input[i];
     }
     return result;
